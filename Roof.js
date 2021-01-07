14596ca170f9ef859ac3565a543f193b0be98fa2
Megha3970 <73172114+Megha3970@users.noreply.github.com>
2021-01-07 20:24:57 +1100

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,30 @@
+class Roof
+{
+    constructor(x,y,width,height,options)
+    {
+        {
+            var options ={
+                isStatic:true
+            
+            }
+            this.x=x;
+            this.y=y;
+            this.width=width;
+            this.height=height;
+            this.body=Bodies.rectangle(this.x,this.y, this.width, this.height, options);
+            World.add(world, this.body);
+            }
+
+    }
+
+    display()
+    {
+        var groundPos = this.body.position;
+        push()
+        translate (groundPos.x, groundPos.y);
+        rectMode (CENTER);
+        fill ("grey");
+        rect (0,0,this.width, this.height);
+        pop();
+    }
+}
